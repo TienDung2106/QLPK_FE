6de80187db747d365e6203b047c84eccab679a44
TienDung2106 <178668020+TienDung2106@users.noreply.github.com>
2026-09-07 09:45:35 +0700

    thay email

--- src/components/Header/TopBar.tsx
+++ src/components/Header/TopBar.tsx
@@ -41,7 +41,7 @@ export const TopBar: React.FC = () => {
             </div>
             <div className="topbar-text">
               <span className="topbar-label">Email</span>
-              <a href="mailto:phongkhamdalieu@gmail.com" className="topbar-value">phongkhamdalieu@gmail.com</a>
+              <a href="mailto:phongkhamdalieu@gmail.com" className="topbar-value">tronghoangafk123@gmail.com</a>
             </div>
           </div>
         </div>
